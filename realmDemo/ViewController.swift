2b4a331306e764f6eb1464d7b16883e7746f228c
Özgür Elmasli <ozgur968@gmail.com>
2019-03-06 13:18:49 +0300

    Initial Commit

---
+++ realmDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmDemo
+//
+//  Created by Özgür  Elmaslı on 6.03.2019.
+//  Copyright © 2019 Özgür  Elmaslı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
